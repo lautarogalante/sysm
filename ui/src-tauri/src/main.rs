24deb2b3ba7a8cdd992fb7079ec53df4c3f4c891
Lautaro Galante <lautaro9galante@gmail.com>
2024-02-12 02:39:10 -0300

    Update path for spawn Go binary

--- ui/src-tauri/src/main.rs
+++ ui/src-tauri/src/main.rs
@@ -10,12 +10,11 @@ fn greet(name: &str) -> String {
 use std::process::Command;
 
 fn main() {
-    let mut _child = Command::new("/usr/bin/sysm")
+    let mut _child = Command::new("/usr/local/bin/sysm")
         .spawn()
         .expect("Error al ejecutar el binario");
 
     let _res = tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!());
-
 }
